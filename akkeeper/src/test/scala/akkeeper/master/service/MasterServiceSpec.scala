a6ca36dcfeb52f8d9ccd0990569e7812df8d882f
Iaroslav Zeigerman <iaroslav@datarobot.com>
2017-02-27 19:14:23 -0500

    Isolate Actor Systems for the Master Service tests

--- akkeeper/src/test/scala/akkeeper/master/service/MasterServiceSpec.scala
+++ akkeeper/src/test/scala/akkeeper/master/service/MasterServiceSpec.scala
@@ -28,21 +28,10 @@ import org.scalamock.scalatest.MockFactory
 import org.scalatest._
 import scala.concurrent.Future
 import scala.concurrent.duration._
+import MasterServiceSpec._
 import MonitoringServiceSpec._
 
-class MasterServiceSpec(system: ActorSystem) extends TestKit(system)
-  with FlatSpecLike with Matchers with ImplicitSender with MockFactory with ActorTestUtils
-  with BeforeAndAfterAll {
-
-  def this() = this(ActorSystem("MasterServiceSpec",
-    ConfigFactory
-      .load("application-container-test.conf")
-      .withValue("akka.remote.netty.tcp.port", ConfigValueFactory.fromAnyRef(new Integer(0)))))
-
-  override def afterAll(): Unit = {
-    system.shutdown()
-    super.afterAll()
-  }
+class MasterServiceSpec extends FlatSpecLike with Matchers with MockFactory with ActorTestUtils {
 
   "A Master Service" should "initialize successfully and create a new cluster" in {
     val storage = mock[InstanceStorage.Async]
@@ -56,42 +45,50 @@ class MasterServiceSpec(system: ActorSystem) extends TestKit(system)
       .expects(*, *)
       .returns(Seq(Future successful DeploySuccessful(InstanceId("container1"))))
 
-    val service = MasterService.createLocal(system, deployClient, storage)
+    new MasterServiceTestRunner() {
+      override def test(): Unit = {
+        val service = MasterService.createLocal(system, deployClient, storage)
 
-    val getInstances = GetInstances()
-    service ! getInstances
-    val response = expectMsgClass(classOf[InstancesList])
-    response.requestId shouldBe getInstances.requestId
-    response.instanceIds shouldBe empty
+        val getInstances = GetInstances()
+        service ! getInstances
+        val response = expectMsgClass(classOf[InstancesList])
+        response.requestId shouldBe getInstances.requestId
+        response.instanceIds shouldBe empty
 
-    gracefulActorStop(service)
+        gracefulActorStop(service)
+      }
+    }.run()
   }
 
   it should "initialize successfully and join the existing cluster" in {
-    val selfAddr = Cluster(system).selfAddress
-    val instance = createInstanceInfo("container").copy(address = Some(selfAddr))
-    val storage = mock[InstanceStorage.Async]
-    (storage.start _).expects()
-    (storage.stop _).expects()
-    (storage.getInstances _).expects().returns(Future successful Seq(instance.instanceId))
-    (storage.getInstance _).expects(instance.instanceId).returns(Future successful instance)
+    new MasterServiceTestRunner() {
+      override def test(): Unit = {
+        val selfAddr = Cluster(system).selfAddress
+        val instance = createInstanceInfo("container").copy(address = Some(selfAddr))
+        val storage = mock[InstanceStorage.Async]
+        (storage.start _).expects()
+        (storage.stop _).expects()
+        (storage.getInstances _).expects().returns(Future successful Seq(instance.instanceId))
+        (storage.getInstance _).expects(instance.instanceId).returns(Future successful instance)
 
-    val deployClient = mock[DeployClient.Async]
-    (deployClient.start _).expects()
-    (deployClient.stop _).expects()
-    (deployClient.deploy _)
-      .expects(*, *)
-      .returns(Seq(Future successful DeploySuccessful(InstanceId("container1"))))
+        val deployClient = mock[DeployClient.Async]
+        (deployClient.start _).expects()
+        (deployClient.stop _).expects()
+        (deployClient.deploy _)
+          .expects(*, *)
+          .returns(Seq(Future successful DeploySuccessful(InstanceId("container1"))))
 
-    val service = MasterService.createLocal(system, deployClient, storage)
+        val service = MasterService.createLocal(system, deployClient, storage)
 
-    val getInstances = GetInstances()
-    service ! getInstances
-    val response = expectMsgClass(classOf[InstancesList])
-    response.requestId shouldBe getInstances.requestId
-    response.instanceIds.size shouldBe 1
+        val getInstances = GetInstances()
+        service ! getInstances
+        val response = expectMsgClass(classOf[InstancesList])
+        response.requestId shouldBe getInstances.requestId
+        response.instanceIds.size shouldBe 1
 
-    gracefulActorStop(service)
+        gracefulActorStop(service)
+      }
+    }.run()
   }
 
   it should "proxy deploy and container requests" in {
@@ -110,46 +107,70 @@ class MasterServiceSpec(system: ActorSystem) extends TestKit(system)
       .expects(*, *)
       .returns(Seq(Future successful DeploySuccessful(InstanceId("container1"))))
 
-    val service = MasterService.createLocal(system, deployClient, storage)
+    new MasterServiceTestRunner() {
+      override def test(): Unit = {
+        val service = MasterService.createLocal(system, deployClient, storage)
 
-    val getContainers = GetContainers()
-    service ! getContainers
-    val container = expectMsgClass(classOf[ContainersList])
-    container.requestId shouldBe getContainers.requestId
-    container.containers.size shouldBe 2
-    container.containers should contain allOf("container1", "container2")
+        val getContainers = GetContainers()
+        service ! getContainers
+        val container = expectMsgClass(classOf[ContainersList])
+        container.requestId shouldBe getContainers.requestId
+        container.containers.size shouldBe 2
+        container.containers should contain allOf("container1", "container2")
 
-    val deployContainer = DeployContainer("container1", 2)
-    service ! deployContainer
-    val deployResult = expectMsgClass(classOf[DeployedInstances])
-    deployResult.requestId shouldBe deployContainer.requestId
-    deployResult.containerName shouldBe "container1"
-    deployResult.instanceIds.size shouldBe 2
+        val deployContainer = DeployContainer("container1", 2)
+        service ! deployContainer
+        val deployResult = expectMsgClass(classOf[DeployedInstances])
+        deployResult.requestId shouldBe deployContainer.requestId
+        deployResult.containerName shouldBe "container1"
+        deployResult.instanceIds.size shouldBe 2
 
-    gracefulActorStop(service)
+        gracefulActorStop(service)
+      }
+    }.run()
   }
 
   it should "shutdown the Actor system if the init process fails" in {
-    val newSystem = ActorSystem("MasterServiceNegativeTest", system.settings.config)
+    new MasterServiceTestRunner() {
+      override def test(): Unit = {
+        val selfAddr = Cluster(system).selfAddress
+        val instance = createInstanceInfo("container").copy(address = Some(selfAddr))
+        val storage = mock[InstanceStorage.Async]
+        (storage.start _).expects()
+        (storage.stop _).expects()
+        (storage.getInstances _).expects().returns(Future successful Seq(instance.instanceId))
+        (storage.getInstance _)
+          .expects(instance.instanceId)
+          .returns(Future failed new AkkeeperException(""))
 
-    val selfAddr = Cluster(newSystem).selfAddress
-    val instance = createInstanceInfo("container").copy(address = Some(selfAddr))
-    val storage = mock[InstanceStorage.Async]
-    (storage.start _).expects()
-    (storage.stop _).expects()
-    (storage.getInstances _).expects().returns(Future successful Seq(instance.instanceId))
-    (storage.getInstance _)
-      .expects(instance.instanceId)
-      .returns(Future failed new AkkeeperException(""))
+        val deployClient = mock[DeployClient.Async]
+        (deployClient.start _).expects()
+        (deployClient.stop _).expects()
+        (deployClient.stopWithError _).expects(*)
 
-    val deployClient = mock[DeployClient.Async]
-    (deployClient.start _).expects()
-    (deployClient.stop _).expects()
-    (deployClient.stopWithError _).expects(*)
+        MasterService.createLocal(system, deployClient, storage)
 
-    MasterService.createLocal(newSystem, deployClient, storage)
+        system.awaitTermination(3 seconds)
+        system.isTerminated shouldBe true
+      }
+    }.run()
+  }
+}
 
-    newSystem.awaitTermination(3 seconds)
-    newSystem.isTerminated shouldBe true
+object MasterServiceSpec {
+  abstract class MasterServiceTestRunner(system: ActorSystem)
+      extends TestKit(system) with ImplicitSender {
+
+    def this() = this(ActorSystem("MasterServiceSpec-" + System.currentTimeMillis(),
+      ConfigFactory
+        .load("application-container-test.conf")
+        .withValue("akka.remote.netty.tcp.port", ConfigValueFactory.fromAnyRef(new Integer(0)))))
+
+    def test(): Unit
+
+    def run(): Unit = {
+      test()
+      system.shutdown()
+    }
   }
 }
