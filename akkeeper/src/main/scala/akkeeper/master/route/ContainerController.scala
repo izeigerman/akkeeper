8f20eafd173c564c680b957a04dda968b23c255a
Viktor Ershov <krinart@gmail.com>
2019-01-08 21:38:11 -0800

    Remove containers modification API

--- akkeeper/src/main/scala/akkeeper/master/route/ContainerController.scala
+++ akkeeper/src/main/scala/akkeeper/master/route/ContainerController.scala
@@ -20,7 +20,7 @@ import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Route
 import akka.util.Timeout
 import akkeeper.api._
-import akkeeper.common.{ContainerDefinition, ContainerDefinitionJsonProtocol}
+import akkeeper.common.ContainerDefinitionJsonProtocol
 import scala.concurrent.ExecutionContext
 
 class ContainerController(service: ActorRef)(implicit dispatcher: ExecutionContext,
@@ -41,24 +41,11 @@ class ContainerController(service: ActorRef)(implicit dispatcher: ExecutionConte
       path(Segment) { containerName =>
         get {
           handleRequest(service, GetContainer(containerName))
-        } ~
-        patch {
-          entity(as[ContainerDefinition]) { definition =>
-            handleRequest(service, UpdateContainer(definition))
-          }
-        } ~
-        delete {
-          handleRequest(service, DeleteContainer(containerName))
         }
       } ~
       (pathEnd | pathSingleSlash) {
         get {
           handleRequest(service, GetContainers())
-        } ~
-        post {
-          entity(as[ContainerDefinition]) { definition =>
-            handleRequest(service, CreateContainer(definition))
-          }
         }
       }
     }
